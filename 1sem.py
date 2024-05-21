19e0c5e7fcaf9bd761ea24387b739c722a97564d
Kirill Lykov <kirillykov07@gmail.com>
2024-05-21 13:01:25 +0300

    asds

---
+++ 1sem.py
@@ -0,0 +1,7 @@
+# введите два числа и определите большее
+first = input("first")
+second = input("second")
+if first > second:
+    print("первое число больше второго")
+else:
+    print("второе число больше первого")
